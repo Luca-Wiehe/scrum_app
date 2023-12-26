ad0f65da6964cfd398c2846ebd9db2daa53585c3
ge75kel <ge75kel@mytum.de>
2023-12-26 15:08:54 +0100

    Implemented navigation hierarchy

--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -12,11 +12,22 @@ struct ScrumsView: View {
     let scrums: [DailyScrum]
     
     var body: some View {
-        // if we don't have an Identifiable object, we need to `pass id: \.title` as an additional attribute to List
-        List(scrums) {
-            scrum in
-            CardView(scrum: scrum)
+        NavigationStack{
+            // if we don't have an Identifiable object, we need to `pass id: \.title` as an additional attribute to List
+            List(scrums) {
+                scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)){
+                    CardView(scrum: scrum)
+                }
                 .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
         }
     }
 }

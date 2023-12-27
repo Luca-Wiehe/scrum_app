78d36b4ffc6c22448dda912a6155f24756765b61
Luca-Wiehe <luca@wiehe.com>
2023-12-28 00:30:15 +0100

    Minor changes

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
+    // State => this is data source for all children
     @State private var scrums = DailyScrum.sampleData
     
     var body: some Scene {
         WindowGroup {
+            // use $scrums to create state as binding
             ScrumsView(scrums: $scrums)
         }
     }
